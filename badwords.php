bc1e6d78c8c8ba2beabe60d415cddabe194c2fe6
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-14 17:51:15 +0200

    html fix

--- badwords.php
+++ badwords.php
@@ -14,7 +14,25 @@
 </head>
 
 <body>
+
+    <!-- Badword:-->
     <h2>La parola censurata è <?= $censured_word ?> </h2>
+
+    <!-- Original text: -->
+
+    <h2 class="mt-3">Testo originale:</h2>
+    <p><?= $paragraph ?></p>
+
+    <!-- Lenght: -->
+    <h3>Lungo <?= strlen($paragraph) ?> caratteri</h3>
+
+    <!-- Original text: -->
+    <h2 class="mt-3">Testo originale:</h2>
+    <p><?= $final_text ?></p>
+
+    <!-- Lenght: -->
+    <h3>Lungo <?= strlen($final_text) ?> caratteri</h3>
+
 </body>
 
 </html>
