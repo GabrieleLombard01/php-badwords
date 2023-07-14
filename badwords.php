15fbe4ea28862fcd3717d77d620afc627170e360
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-14 17:36:50 +0200

    button add

--- badwords.php
+++ badwords.php
@@ -3,3 +3,18 @@
     $paragraph = $_GET[trim("paragraph")];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Text with bad word</title>
+</head>
+
+<body>
+    <h2>La parola censurata è <?= $censured_word ?> </h2>
+</body>
+
+</html>
